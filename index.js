bd9b444199ac1ea529ce876b10a30d037888ab73
nsluny <nsluny@gmail.com>
2018-03-28 17:00:47 +0300

    добавленны новые фигуры

--- index.js
+++ index.js
@@ -3,15 +3,24 @@
 module.exports = {
 
 	initObjects(fabric = this.getFabric()) {
-		fabric.ISPFabricCanvas          = require('./lib/objects/isp-fabric-canvas.js');
-		fabric.ISPFabricPage            = require('./lib/objects/isp-fabric-page.js');
-		fabric.ISPShapeBookmark         = require('./lib/objects/isp-shape-bookmark.js');
-		fabric.ISPShapeBubble           = require('./lib/objects/isp-shape-bubble.js');
-		fabric.ISPShapeBubbleRound      = require('./lib/objects/isp-shape-bubble-round.js');
-		fabric.ISPShapeCertificate      = require('./lib/objects/isp-shape-certificate.js');
-		fabric.ISPShapeHeart            = require('./lib/objects/isp-shape-heart.js');
-		fabric.ISPShapeStar5            = require('./lib/objects/isp-shape-star-5.js');
-		fabric.ISPTextbox               = require('./lib/objects/isp-textbox.js');
+		fabric.ISPFabricCanvas            = require('./lib/objects/isp-fabric-canvas.js');
+		fabric.ISPFabricPage              = require('./lib/objects/isp-fabric-page.js');
+		fabric.ISPShapeBookmark           = require('./lib/objects/isp-shape-bookmark.js');
+		fabric.ISPShapeBubble             = require('./lib/objects/isp-shape-bubble.js');
+		fabric.ISPShapeBubbleRound        = require('./lib/objects/isp-shape-bubble-round.js');
+		fabric.ISPShapeCertificate        = require('./lib/objects/isp-shape-certificate.js');
+		fabric.ISPShapeHeart              = require('./lib/objects/isp-shape-heart.js');
+		fabric.ISPShapeStar5              = require('./lib/objects/isp-shape-star-5.js');
+		fabric.ISPTextbox                 = require('./lib/objects/isp-textbox.js');
+		fabric.ISPShapePhoneIcon          = require('./lib/objects/isp-shape-phone-icon.js');
+		fabric.ISPShapeLocationIcon       = require('./lib/objects/isp-shape-location-icon.js');
+		fabric.ISPShapeClEnvelopIcon      = require('./lib/objects/isp-shape-cl-envelop-icon.js');
+		fabric.ISPShapeVkIcon             = require('./lib/objects/isp-shape-vk-icon.js');
+		fabric.ISPShapeInstagramIcon      = require('./lib/objects/isp-shape-instagram-icon.js');
+		fabric.ISPShapeInternetIcon       = require('./lib/objects/isp-shape-internet-icon.js');
+		fabric.ISPShapeOdnoklassnikiIcon  = require('./lib/objects/isp-shape-odnoklassniki-icon.js');
+		fabric.ISPShapeFbIcon             = require('./lib/objects/isp-shape-fb-icon.js');
+		fabric.ISPShapePlaceholderIcon    = require('./lib/objects/isp-shape-placeholder-icon.js');
 
 		return fabric;
 	},
@@ -35,7 +44,7 @@ module.exports = {
 
 
 	getFabric() {
-		let fabric = require('fabric');
+		let fabric   = require('fabric');
 
 		// модуль по разному экспортируется для nodejs и браузера
 		return fabric.fabric || fabric;
